fd123678d82e64b5cf5ffbb112e47155ea23322e
Aquamenti8 <lea.vasselle@laposte.net>
2018-03-24 10:30:59 +0100

    Fixed SnapTopoint and Added Gun
    -gun is following player
    -gun's rotation is to improve

---
+++ Assets/Resources/Script/GunController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    /*
+     * le gun suis le personnage, et suit l'angle de la souris
+     * 
+     * 
+     * 
+     */
+    public GameObject player;
+    public GameObject[] playerList;
+    
+	// Use this for initialization
+	void Start () {
+        /*set le player en fonction de qui est le joueur courant (check les instance
+         * de player et regarde la variable "isControlledPlayer"
+         * si c'est le cas prend le gameobject et attache le gun a aca
+         */
+        //PhotonView.isMine;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(player == null)          //GET PLAYER
+        {
+            playerList = GameObject.FindGameObjectsWithTag("Player");
+            for (int i = 0; i < playerList.Length; i++)
+            {
+                if (playerList[i].GetComponent<PlayerController>().isControlledPlayer)
+                {
+                    player = playerList[i];
+                }
+            }
+            if (player != null)
+            {
+                transform.position = player.transform.position;
+                transform.parent = player.transform;
+            }
+        }
+        if (player != null)       //ROTATE TO MOUSE
+            {
+
+            FaceMouse();
+        }
+    }
+
+    void FaceMouse()
+    {
+        /*
+        // Get Angle in Radians
+        float AngleRad = Mathf.Atan2(Input.mousePosition.y - transform.position.y, Input.mousePosition.x - transform.position.x);
+        // Get Angle in Degrees
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        // Rotate Object
+        this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+        */
+
+        Vector3 mousePosition = Input.mousePosition;
+        Vector2 direction = new Vector2(
+                mousePosition.x - transform.position.x,
+                mousePosition.y - transform.position.y
+            );
+
+        transform.right = direction;
+    }
+
+    
+}
--- Assets/Resources/Script/PlayerController.cs
+++ Assets/Resources/Script/PlayerController.cs
@@ -10,10 +10,23 @@ public class PlayerController : MonoBehaviour {
     PhotonView view;
     public Animator anim;
 
+    public bool isControlledPlayer;
+
+    //For Shift
+    private GameObject[] targetPoints;
+    private GameObject minTarget;
+    private Vector2 directionVersPoint;
+
     // Use this for initialization
     void Start() {
         view = GetComponent<PhotonView>();
         anim = GetComponent<Animator>();
+
+        if (view.isMine)
+        {
+            isControlledPlayer = true;
+        }
+        else isControlledPlayer = false;
     }
 
     // Update is called once per frame
@@ -22,7 +35,7 @@ public class PlayerController : MonoBehaviour {
         if (view.isMine)
         {
             GetInput();
-
+            GetShift();
             Move();
         }
 
@@ -35,6 +48,7 @@ public class PlayerController : MonoBehaviour {
 
     private void GetInput()
     {
+            //TODO changer le truc anim.SetInteger, le mettre en fonction du vecteur de direction.
         direction = Vector2.zero;
         anim.SetInteger("state", 0);
         if (Input.GetKey(KeyCode.Z))
@@ -59,5 +73,64 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
+    private void GetShift()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) //set la target
+        {
+            Debug.Log("Shift");
+            targetPoints = GameObject.FindGameObjectsWithTag("TargetPoint");
+
+            if (targetPoints != null)//si il ey a des cibles dans la salle
+            {
+                float distance;     //initialise la variable qui contient la distance joueur point
+                float minDistance = 100; // est aussi la distance min pour que ce soit reperé!
+                minTarget = null;        //rezet la target
+
+                for (int i = 0; i < targetPoints.Length; i++)
+                {
+                    distance = Vector2.Distance(transform.position, targetPoints[i].transform.position);
+                    
+                    if (distance < minDistance)
+                    {
+                        //verifier la direction, si direction et direction vers objt sont trop differents ca passe pas
+                        directionVersPoint= new Vector2(transform.position.x- targetPoints[i].transform.position.x, transform.position.y - targetPoints[i].transform.position.y); //pos x2-x1,y2-y1  
+                        Debug.Log("DirVerspoint = " + directionVersPoint);
+                        Debug.Log("DirVerspointNorm = " + directionVersPoint.normalized);
+                        Debug.Log("Move = " + Vector2.MoveTowards(transform.position, targetPoints[i].transform.position, 1000 * Time.deltaTime));
+                        Debug.Log("MoveNorm = " + Vector2.MoveTowards(transform.position, targetPoints[i].transform.position, 1000 * Time.deltaTime).normalized);
+
+                        float directionDiff = Vector2.Dot(directionVersPoint.normalized, direction.normalized);
+                        Debug.Log("directionDiff" + directionDiff);
+
+                        if (directionDiff < -0.3f)
+                        {
+                            //calculer la distance du target point au joueur, la plus petite distance restera.
+                            minDistance = distance;
+                            minTarget = targetPoints[i];
+                        }
+                    }
+                }
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftShift)) //se deplace vers la target
+        {
+            //se deplace vers minTarget
+            if (minTarget != null)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, minTarget.transform.position, 1000 * Time.deltaTime);
+            }
+        }
+        
+    }
+    //Summary
+    /*
+     * si on appuit sur shift:
+     * on cherche si un point est pres du joueur, en particulier dans la direction du joueur
+     * si il y en a un:
+     * le joueur n'est plus libre de ses mouvements
+     * le joueur se deplace tres rapidement sur le point
+     * 
+     * 
+     */
 
 }
