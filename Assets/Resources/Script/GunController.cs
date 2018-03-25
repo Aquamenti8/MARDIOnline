6cd28af673ea38f31bfe7ddc5580c35c88e941dc
Aquamenti8 <lea.vasselle@laposte.net>
2018-03-26 01:28:33 +0200

    GunFire
    -rotation is awful

--- Assets/Resources/Script/GunController.cs
+++ Assets/Resources/Script/GunController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//[ExecuteInEditMode]
 public class GunController : MonoBehaviour {
 
     /*
@@ -12,9 +12,13 @@ public class GunController : MonoBehaviour {
      */
     public GameObject player;
     public GameObject[] playerList;
+    public GameObject laser;
+    public float laserLength;
+    float angleShoot;
+
     
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         /*set le player en fonction de qui est le joueur courant (check les instance
          * de player et regarde la variable "isControlledPlayer"
          * si c'est le cas prend le gameobject et attache le gun a aca
@@ -25,7 +29,8 @@ public class GunController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(player == null)          //GET PLAYER
+        
+        if (player == null)          //GET PLAYER
         {
             playerList = GameObject.FindGameObjectsWithTag("Player");
             for (int i = 0; i < playerList.Length; i++)
@@ -43,7 +48,8 @@ public class GunController : MonoBehaviour {
         }
         if (player != null)       //ROTATE TO MOUSE
             {
-
+            
+            GetGunFire();
             FaceMouse();
         }
     }
@@ -61,12 +67,35 @@ public class GunController : MonoBehaviour {
 
         Vector3 mousePosition = Input.mousePosition;
         Vector2 direction = new Vector2(
-                mousePosition.x - transform.position.x,
-                mousePosition.y - transform.position.y
+                mousePosition.x - player.transform.position.x,
+                mousePosition.y - player.transform.position.y
             );
 
         transform.right = direction;
     }
 
+    void GetGunFire()
+    {
+        /*
+         * si click gauche est pressé
+         * Tire un laser dans la direction de la souris
+         */
+         if (Input.GetMouseButtonDown(0))
+        {
+            //Tire un laser en direction de la souri!
+            Vector3 mousePosition = Input.mousePosition;
+            Vector2 direction = new Vector2(
+                mousePosition.x - transform.position.x,
+                mousePosition.y - transform.position.y
+            );
+            angleShoot = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            laserLength = Vector3.Distance(transform.position, mousePosition);
+
+            GameObject laserInstance = (GameObject)Instantiate(laser, transform.position, Quaternion.Euler(0, 0, angleShoot));
+            laserInstance.transform.localScale = new Vector3(laserLength, 100, 1);
+
+        }
+    }
     
 }
